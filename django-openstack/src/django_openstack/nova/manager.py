21206451d3927ac03888f3e43d5aa5485cdf8c15
canarie <dair-support@canarie.ca>
2011-07-05 11:04:15 -0600

    Fixed support for multiple regions

--- django-openstack/src/django_openstack/nova/manager.py
+++ django-openstack/src/django_openstack/nova/manager.py
@@ -42,8 +42,8 @@ class ProjectManager(object):
         openstack = get_nova_admin_connection()
         return openstack.connection_for(self.username,
                                    self.projectname,
-                                   clc_url=settings.NOVA_DEFAULT_ENDPOINT,
-                                   region=settings.NOVA_DEFAULT_REGION)
+                                   clc_url=self.region['endpoint'],
+                                   region=self.region['name'])
 
     def get_zip(self):
         """
